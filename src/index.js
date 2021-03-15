44c53ee135dc892c8b315600108bfdc09f064a13
Marfusha-91 <71018868+Marfusha-91@users.noreply.github.com>
2021-03-15 11:13:33 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function toReadable (number) {
-  
+    number = number.toString();
+    number = number.replace([\, ]/g,'');
+    if ( number != parseFloat(number)) return 'not a number';
+    let x = number.indexOf('.');  
+    if (x == -1) 
+     x = number.length;
+   if ( x > 15)  return 'big';
+                             
 }
